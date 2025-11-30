ffa47a0f312cd65c78c3d97ee8199bc2767fb66e
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2025-11-30 12:47:10 +0000

    new changes in this

--- vite-project/src/component/CreateTodo.jsx
+++ vite-project/src/component/CreateTodo.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import '../index.css';
 
 const CreateTodo = () => {
-    return <h1> Create new Todo from here </h1>;
+    const styles = {
+        backgroundColor: 'white',
+        textAlign:'center',
+        marginLeft: '300px',
+    }
+
+    return <h1 style={styles}> Create new Todo from here </h1>;
 }
 
 export default CreateTodo;
